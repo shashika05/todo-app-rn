e0698eebd9af84619f9053e0741d3a59aebeb4d5
Shashika Raveen <32318854+shashika05@users.noreply.github.com>
2023-03-31 18:51:46 +0000

    added axios

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
 import { StatusBar } from "expo-status-bar";
+import { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import axios from "axios";
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:9898/todos")
+      .then((res) => setTodos(res.data))
+      .catch((err) => console.log(err));
+    console.log(todos);
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Todo App</Text>
